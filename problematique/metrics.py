ad4308646cae2f8c71a9e26034066da764b7f18c
GabDucas <gabducas4@gmail.com>
2026-03-17 19:35:50 -0400

    metrics

--- problematique/metrics.py
+++ problematique/metrics.py
@@ -3,16 +3,37 @@
 # Hivers 2021
 import numpy as np
 
-def edit_distance(x,y):
+def edit_distance(a,b):
     # Calcul de la distance d'édition
+
+    m, n = len(a), len(b)
+    D = np.zeros((m+1, n+1), dtype=int)
     
-    # TODO
-    
-    return None
+    for coordinates, value in np.ndenumerate(D):
+        x, y = coordinates
+        if x == 0:
+            D[coordinates] = y
+        elif y == 0:
+            D[coordinates] = x
+        else:
+            min1 = D[x,y-1] + 1
+            min2 = D[x-1,y] + 1
+            if a[x-1] == b[y-1]: 
+                min3 = D[x-1, y-1]
+            else:
+                min3 = D[x-1, y-1] + 1
+            D[coordinates] = np.min([min1, min2, min3])
+
+    return D[m,n]
 
 def confusion_matrix(true, pred, ignore=[]):
-    # Calcul de la matrice de confusion
 
-    # TODO
+    confusion_matrix = np.zeros((len(set(true)), len(set(pred))), dtype=int)
 
-    return None
+    for i in range(len(true)):
+        for j in range(len(pred)):
+            if true[i] == pred[j] and true[i] not in ignore:
+                confusion_matrix[true[i], pred[j]] += 1
+
+       
+    return confusion_matrix
