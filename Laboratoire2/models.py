86c0cef68bd1e3bc2b97980055161c34e4904ab0
GabDucas <gabducas4@gmail.com>
2026-03-17 21:53:24 -0400

    models et première partie du main

--- Laboratoire2/models.py
+++ Laboratoire2/models.py
@@ -83,77 +83,61 @@ class Seq2seq_attn(nn.Module):
         self.dict_size = dict_size
         self.max_len = max_len
 
-        # Définition des couches du rnn
-        self.fr_embedding = nn.Embedding(self.dict_size['fr'], n_hidden)
-        self.en_embedding = nn.Embedding(self.dict_size['en'], n_hidden)
-        self.encoder_layer = nn.GRU(n_hidden, n_hidden, n_layers, batch_first=True)
+        self.encoder_layer = nn.GRU(2, n_hidden, n_layers, batch_first=True)
+        
+        # Embedding décodeur
+        self.embedding = nn.Embedding(dict_size, n_hidden)
+
+        # Décodeur
         self.decoder_layer = nn.GRU(n_hidden, n_hidden, n_layers, batch_first=True)
 
-        # Définition de la couche dense pour l'attention
+        # Couches attention
         self.att_combine = nn.Linear(2*n_hidden, n_hidden)
         self.hidden2query = nn.Linear(n_hidden, n_hidden)
 
-        # Définition de la couche dense pour la sortie
-        self.fc = nn.Linear(n_hidden, self.dict_size['en'])
-        self.to(device)
+        # Sortie
+        self.fc = nn.Linear(n_hidden, dict_size)
         
     def encoder(self, x):
-        #Encodeur
 
-        # ---------------------- Laboratoire 2 - Question 4 - Début de la section à compléter -----------------
-        fr_embedded = self.fr_embedding(x)
-        out, hidden = self.encoder_layer(fr_embedded)
-        
-        # ---------------------- Laboratoire 2 - Question 4 - Début de la section à compléter -----------------
-
+        out, hidden = self.encoder_layer(x)
         return out, hidden
 
     def attentionModule(self, query, values):
-        # Module d'attention
-
-        # Couche dense à l'entrée du module d'attention
         query = self.hidden2query(query)
 
-        # Attention
-
-        # ---------------------- Laboratoire 2 - Question 4 - Début de la section à compléter -----------------
-        # similarity = query @ values
-        attention = torch.bmm(query, values.permute(0, 2, 1))
+        attention = torch.bmm(query, values.permute(0,2,1))
         attention_weights = torch.softmax(attention[:,0,:], dim=1)
-        attention_weights_repeat = attention_weights[:,:,None].repeat(1,1,self.n_hidden)
-        attention_output = torch.sum(attention_weights_repeat*values, dim=1)
-    
 
-        # ---------------------- Laboratoire 2 - Question 4 - Début de la section à compléter -----------------
+        attention_weights_repeat = attention_weights[:,:,None].repeat(1,1,self.n_hidden)
+        attention_output = torch.sum(attention_weights_repeat * values, dim=1)
 
         return attention_output, attention_weights
 
     def decoderWithAttn(self, encoder_outs, hidden):
-        # Décodeur avec attention
+        max_len = self.max_len
+        batch_size = hidden.shape[1]
 
-        # Initialisation des variables
-        max_len = self.max_len['en'] # Longueur max de la séquence anglaise (avec padding)
-        batch_size = hidden.shape[1] # Taille de la batch
-        vec_in = torch.zeros((batch_size, 1)).to(self.device).long() # Vecteur d'entrée pour décodage 
-        vec_out = torch.zeros((batch_size, max_len, self.dict_size['en'])).to(self.device) # Vecteur de sortie du décodage
-        attention_weights = torch.zeros((batch_size, self.max_len['fr'], self.max_len['en'])).to(self.device) # Poids d'attention
+        vec_in = torch.full((batch_size,1),
+                            self.symb2int['<sos>'],
+                            dtype=torch.long).to(self.device)
 
-        # Boucle pour tous les symboles de sortie
+        vec_out = torch.zeros((batch_size, max_len, self.dict_size)).to(self.device)
+        attention_weights = torch.zeros((batch_size, encoder_outs.shape[1], max_len)).to(self.device)
+
         for i in range(max_len):
 
-            # ---------------------- Laboratoire 2 - Question 4 - Début de la section à compléter -----------------
-            
-            en_embedded = self.en_embedding(vec_in)
-            buffer, hidden = self.decoder_layer(en_embedded, hidden)
+            embedded = self.embedding(vec_in)
+            buffer, hidden = self.decoder_layer(embedded, hidden)
+
             attention_out, attention_weight_buff = self.attentionModule(buffer, encoder_outs)
             attention_weights[:,:,i] = attention_weight_buff
+
             out = self.att_combine(torch.cat([buffer[:,0,:], attention_out], dim=1))
             out_lin = self.fc(out)
 
-            vec_out[:,i,:] = out_lin.clone()
-            vec_in = torch.argmax(vec_out[:,i:i+1,:], dim=2)
-
-            # ---------------------- Laboratoire 2 - Question 4 - Début de la section à compléter -----------------
+            vec_out[:,i,:] = out_lin
+            vec_in = torch.argmax(out_lin.unsqueeze(1), dim=2)
 
         return vec_out, hidden, attention_weights
 
--- problematique/main.py
+++ problematique/main.py
@@ -36,52 +36,114 @@ if __name__ == '__main__':
 
     # Instanciation de l'ensemble de données
     # TODO
+    dataset = HandwrittenWords(n_samp=4000, samplelen=[6,10])
 
     
     # Séparation de l'ensemble de données (entraînement et validation)
     # TODO
-   
+    dataset_train, dataset_val = torch.utils.data.random_split(dataset, [3000, 1000])
 
     # Instanciation des dataloaders
     # TODO
-
+    dataload_train = DataLoader(dataset_train, batch_size=100, shuffle=True, num_workers=n_workers)
+    dataload_val = DataLoader(dataset_val, batch_size=100, shuffle=False, num_workers=n_workers)
 
     # Instanciation du model
     # TODO
-
-
+    model = trajectory2seq(
+        hidden_dim=64,
+        n_layers=1,
+        device=device,
+        symb2int=dataset.symb2int,
+        int2symb=dataset.int2symb,
+        dict_size=dataset.dict_size,
+        maxlen=dataset.max_len
+    ).to(device)
     # Initialisation des variables
     # TODO
+
+    train_loss_list = []
+    val_loss_list = []
 
     if trainning:
 
         # Fonction de coût et optimizateur
         # TODO
+        criterion = nn.CrossEntropyLoss(ignore_index=dataset.symb2int['<pad>'])
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
         for epoch in range(1, n_epochs + 1):
             # Entraînement
             # TODO
-            
+
+            model.train()
+            running_loss_train = 0
+
+            for batch_idx, data in enumerate(dataload_train):
+                # Formatage des données
+                input_seq, target_seq = data
+                input_seq = input_seq.to(device).float()
+                target_seq = target_seq.to(device).long()
+
+                optimizer.zero_grad() 
+
+                output, hidden, attn = model(input_seq)
+
+                loss = criterion(output.view((-1, model.dict_size)), target_seq.view(-1))
+                
+                loss.backward() 
+                optimizer.step() 
+
+                running_loss_train += loss.item()
+            train_loss = running_loss_train / len(dataload_train)
             # Validation
             # TODO
 
+            model.eval()
+            running_loss_val = 0
+            for batch_idx, data in enumerate(dataload_val):
+                with torch.no_grad:
+                    input_seq, target_seq = data
+                    input_seq = input_seq.to(device).long()
+                    target_seq = target_seq.to(device).long()
+
+                    loss = criterion(
+                    output.view(-1, dataset.dict_size),
+                    target_seq.view(-1)
+                )
+
+                    running_loss_val += loss.item()
+
+                    val_loss = running_loss_val / len(dataload_val)
+                    
             # Ajouter les loss aux listes
             # TODO
-
+            train_loss_list.append(train_loss)
+            val_loss_list.append(val_loss)
             # Enregistrer les poids
             # TODO
 
+            ###################CHATGPT?????????#############################
+            if epoch == 1 or val_loss < best_val_loss:
+                best_val_loss = val_loss
+                torch.save(model.state_dict(), "best_model.pt")
+            ################################################################
 
             # Affichage
             if learning_curves:
-                # visualization
-                # TODO
-                pass
+                plt.figure()
+                plt.plot(train_loss_list, label="Train")
+                plt.plot(val_loss_list, label="Validation")
+                plt.xlabel("Epoch")
+                plt.ylabel("Loss")
+                plt.legend()
+                plt.title("Learning Curves")
+                plt.show()
 
     if test:
         # Évaluation
         # TODO
-
+        
         # Charger les données de tests
         # TODO
 
